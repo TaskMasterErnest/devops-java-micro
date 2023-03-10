90b218dfde4464c2fd91abd730d97b60de628f5f
TaskMasterErnest <ernestklu08@gmail.com>
2023-03-10 03:45:33 +0000

    modified groovy script

--- script.groovy
+++ script.groovy
@@ -1,13 +1,13 @@
 def testImage() {
     echo "testing the app ..."
-    sh "mvn test"
+    sh 'mvn test'
 }   
 
 def buildImage(string imageName) {
     echo "building the docker image..."
     withCredentials([
         usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t $imageName .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push $imageName"
 }
