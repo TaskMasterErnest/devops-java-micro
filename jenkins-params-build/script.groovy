dd2ae60e859ded4bfb39ae33155a31ea2b1d5d05
TaskMasterErnest <ernestklu08@gmail.com>
2023-03-21 21:58:44 +0000

    modified the jenkinsfile to add dynamic increments + build docker image

--- jenkins-params-build/script.groovy
+++ jenkins-params-build/script.groovy
@@ -3,6 +3,11 @@ def testImage() {
     sh "mvn test" 
 }   
 
+def buildImage() {
+    echo 'building the docker image ...'
+    sh "docker build -t ernestklu/${IMAGE_NAME} ."
+}
+
 def deployImage() {
     echo "deploying build..."
     echo "This is the third webhook branch .."
